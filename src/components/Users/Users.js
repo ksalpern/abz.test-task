e5d30a0e91ffdc6f2e19261bc258e913ae7f6afd
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-14 09:32:12 +0200

    update Users component, add Form

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import Button from '../UIcomponents/Button/Button'
 import UserCard from './UserCard/UserCard'
 import './Users.scss'
 
 const Users = ({ users }) => {
   return (
     <div className='users'>
-      <h2>Working with GET request</h2>
-      <div className="users__items">
-        {users?.map(user => (
-          <UserCard key={user.registration_timestamp} user={user} />
-        ))}
+      <div className="container">
+        <h2>Working with GET request</h2>
+        <div className="users__items">
+          {users?.map(user => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </div>
+        <Button buttonText='Show more' buttonClass='btn' buttonLink='#' />
       </div>
     </div>
   )
